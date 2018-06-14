6ba032bf2082a78e0307a5984b6641b95249d4bf
José Manuel Palau Alegría <coveromaki@gmail.com>
2018-06-14 17:18:53 +0200

    Minuto 24:22

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+//Middlewares
+
+//Routes
+
+//Starting server
+app.listen(app.get('port'), () => {
+    console.log('server on port ' + app.get('port'));
+})
