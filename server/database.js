5fb8d1cc3ce5df714ac5ee0366473b2f3142cc32
José Manuel Palau Alegría <coveromaki@gmail.com>
2018-06-18 10:07:29 +0200

    minuto 36:39

--- server/database.js
+++ server/database.js
@@ -1,1 +1,6 @@
-
+const mongoose = require('mongoose')
+const URI = 'mongodb://localhost:27017/mean-crud'
+mongoose.connect(URI)
+    .then(db => console.log("db is connected"))
+    .catch(err => console.log(err));
+module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const {mongoose} = require  ("./database.js")
 //Settings
 app.set('port', process.env.PORT || 3000);
 //Middlewares 
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(require('./routes/employee.routes.js'));
 //Routes
 
 //Starting server
